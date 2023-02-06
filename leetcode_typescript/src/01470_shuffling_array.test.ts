e3c37b3bbb2f699327c9b298f2b7df927dddc222
Artem Lebedev <72556276+rulila52@users.noreply.github.com>
2023-02-06 16:54:37 +0300

    ✅ 1470. Shuffle the Array. Time comparison  https://leetcode.com/problems/shuffle-the-array/

--- leetcode_typescript/src/01470_shuffling_array.test.ts
+++ leetcode_typescript/src/01470_shuffling_array.test.ts
@@ -5,4 +5,15 @@ Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,.
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].`, () => {
     expect(shuffle1([1, 3, 5, 2, 4, 6], 3)).toEqual([1, 2, 3, 4, 5, 6]);
     expect(shuffle2([1, 3, 5, 2, 4, 6], 3)).toEqual([1, 2, 3, 4, 5, 6]);
+
+    const n = 100000;
+    const arr = Array(n).fill(1);
+
+    console.time("Time of 1nd method");
+    shuffle1(arr, n);
+    console.timeEnd("Time of 1nd method");
+
+    console.time("Time of 2nd method");
+    shuffle2(arr, n);
+    console.timeEnd("Time of 2nd method");
 });
