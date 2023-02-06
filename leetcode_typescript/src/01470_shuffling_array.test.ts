d27b5c03ad53abff73d0a2570bbd2d51b2a0ef7d
Artem Lebedev <72556276+rulila52@users.noreply.github.com>
2023-02-06 16:11:30 +0300

    ✅ 1470. Shuffle the Array. 2nd solution (🥳) (Accepted - by myself) 🔗 https://leetcode.com/problems/shuffle-the-array/

--- leetcode_typescript/src/01470_shuffling_array.test.ts
+++ leetcode_typescript/src/01470_shuffling_array.test.ts
@@ -1,7 +1,8 @@
-import { shuffle } from "./01470_shuffling_array";
+import { shuffle1, shuffle2 } from "./01470_shuffling_array";
 
 it(`1470. Shuffle the Array
 Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].`, () => {
-    expect(shuffle([1, 3, 5, 2, 4, 6], 3)).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(shuffle1([1, 3, 5, 2, 4, 6], 3)).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(shuffle2([1, 3, 5, 2, 4, 6], 3)).toEqual([1, 2, 3, 4, 5, 6]);
 });
--- leetcode_typescript/src/01470_shuffling_array.ts
+++ leetcode_typescript/src/01470_shuffling_array.ts
@@ -1,7 +1,16 @@
-export function shuffle(nums: number[], n: number): number[] {
+export function shuffle1(nums: number[], n: number): number[] {
     for (let i = 0; i < n - 1; i++) {
         nums.splice(i * 2 + 1, 0, nums[n + i]);
         nums.splice(n + i + 1, 1);
     }
     return nums;
+}
+
+export function shuffle2(nums: number[], n: number): number[] {
+    let result = [];
+    for (let i = 0; i < n; i++) {
+        result.push(nums[i]);
+        result.push(nums[i + n]);
+    }
+    return result;
 }
