893bb1f7decc0955aec59b3f45ccc63752aaf9d3
Artem Lebedev <72556276+rulila52@users.noreply.github.com>
2023-02-07 12:14:34 +0300

    Adding links

--- leetcode_typescript/src/01137_tribonacci.test.ts
+++ leetcode_typescript/src/01137_tribonacci.test.ts
@@ -3,7 +3,8 @@ import tribonacci from "./01137_tribonacci";
 it(`1137. N-th Tribonacci Number
 The Tribonacci sequence Tn is defined as follows: 
 T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
-Given n, return the value of Tn`, () => {
+Given n, return the value of Tn
+More: https://leetcode.com/problems/n-th-tribonacci-number/`, () => {
     expect(tribonacci(0)).toEqual(0);
     expect(tribonacci(1)).toEqual(1);
     expect(tribonacci(2)).toEqual(1);
--- leetcode_typescript/src/01470_shuffling_array.test.ts
+++ leetcode_typescript/src/01470_shuffling_array.test.ts
@@ -2,7 +2,8 @@ import { shuffle1, shuffle2 } from "./01470_shuffling_array";
 
 it(`1470. Shuffle the Array
 Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
-Return the array in the form [x1,y1,x2,y2,...,xn,yn].`, () => {
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+More: https://leetcode.com/problems/shuffle-the-array/`, () => {
     expect(shuffle1([1, 3, 5, 2, 4, 6], 3)).toEqual([1, 2, 3, 4, 5, 6]);
     expect(shuffle2([1, 3, 5, 2, 4, 6], 3)).toEqual([1, 2, 3, 4, 5, 6]);
 
