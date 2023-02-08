1d37dedfd5714fd3c98d09172fb89137f0515a48
Artem Lebedev <72556276+rulila52@users.noreply.github.com>
2023-02-08 10:27:47 +0300

    Correcting tests

--- leetcode_typescript/src/00940_fruit_into_baskets.test.ts
+++ leetcode_typescript/src/00940_fruit_into_baskets.test.ts
@@ -1,17 +1,35 @@
 import { totalFruit } from "./00940_fruit_into_baskets";
 
-it(`✅✅ 904. Fruit Into Baskets
-Given the integer array fruits, return the maximum number of fruits you can pick. 
-fruits[i] is the type of fruit the ith tree produces.
-Some strict rules that you must follow:
-- You only have two baskets, and each basket can only hold a single type of fruit.
-- Starting from any tree, you must pick exactly one fruit from every tree while moving to the right.
-- Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
-More: https://leetcode.com/problems/fruit-into-baskets/`, () => {
-    expect(totalFruit([0])).toEqual(1);
-    expect(totalFruit([0, 1, 2, 2])).toEqual(3);
-    expect(totalFruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4])).toEqual(5);
-    expect(totalFruit([3, 3, 3, 1, 4])).toEqual(4);
-    expect(totalFruit([1, 0, 1, 4, 1, 4, 1, 2, 3])).toEqual(5);
-    expect(totalFruit([0, 1, 6, 6, 4, 4, 6])).toEqual(5);
+// ✅✅ 904. Fruit Into Baskets
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+// fruits[i] is the type of fruit the ith tree produces.
+// Some strict rules that you must follow:
+// - You only have two baskets, and each basket can only hold a single type of fruit.
+// - Starting from any tree, you must pick exactly one fruit from every tree while moving to the right.
+// - Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// More: https://leetcode.com/problems/fruit-into-baskets/
+describe("Fruit Into Baskets", () => {
+    it(`one tree test, should return 1 since there is only one tree`, () => {
+        expect(totalFruit([0])).toEqual(1);
+    });
+
+    it(`should return 4 since we can collect from [{3, 3, 3, 1}, 4]`, () => {
+        expect(totalFruit([3, 3, 3, 1, 4])).toEqual(4);
+    });
+
+    it(`should return 3 since we can collect from [0, {1, 2, 2}]`, () => {
+        expect(totalFruit([0, 1, 2, 2])).toEqual(3);
+    });
+
+    it(`should return 5 since we can collect from [3, 3, 3, {1, 2, 1, 1, 2}, 3, 3, 4]`, () => {
+        expect(totalFruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4])).toEqual(5);
+    });
+
+    it(`should return 5 since we can collect from [1̶, 0, {1, 4, 1, 4, 1}, 2, 3], BUT NOT FROM FIRST TREE`, () => {
+        expect(totalFruit([1, 0, 1, 4, 1, 4, 1, 2, 3])).toEqual(5);
+    });
+
+    it(`should return 5 since we can collect from [0, 1, {6, 6, 4, 4, 6}]`, () => {
+        expect(totalFruit([0, 1, 6, 6, 4, 4, 6])).toEqual(5);
+    });
 });
--- leetcode_typescript/src/01137_tribonacci.test.ts
+++ leetcode_typescript/src/01137_tribonacci.test.ts
@@ -1,14 +1,22 @@
 import tribonacci from "./01137_tribonacci";
 
-it(`✅ 1137. N-th Tribonacci Number
-The Tribonacci sequence Tn is defined as follows: 
-T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
-Given n, return the value of Tn
-More: https://leetcode.com/problems/n-th-tribonacci-number/`, () => {
-    expect(tribonacci(0)).toEqual(0);
-    expect(tribonacci(1)).toEqual(1);
-    expect(tribonacci(2)).toEqual(1);
-    expect(tribonacci(3)).toEqual(2);
-    expect(tribonacci(4)).toEqual(4);
-    expect(tribonacci(5)).toEqual(7);
+// ✅ 1137. N-th Tribonacci Number
+// The Tribonacci sequence Tn is defined as follows:
+// T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+// Given n, return the value of Tn
+// More: https://leetcode.com/problems/n-th-tribonacci-number/
+describe("N-th Tribonacci Number", () => {
+    it(`0-7 test, should return T0 = 0, T1 = 1, T2 = 1, 
+    T3 = 0 + 1 + 1 = 2, T4 = 1 + 1 + 2 = 4, T5 = 1 + 2 + 4 = 7`, () => {
+        expect(tribonacci(0)).toEqual(0);
+        expect(tribonacci(1)).toEqual(1);
+        expect(tribonacci(2)).toEqual(1);
+        expect(tribonacci(3)).toEqual(2);
+        expect(tribonacci(4)).toEqual(4);
+        expect(tribonacci(5)).toEqual(7);
+    });
+
+    it(`Test on big data, must be executed within the normal time and return T37 = 2082876103`, async () => {
+        expect(tribonacci(37)).toEqual(2082876103);
+    });
 });
